3c119cb3f0d4d6b7212b2bc77e83495d31775f3d
Jonah Meijers (GitHub) <jonahmeijers97@gmail.com>
2023-03-04 15:33:52 +0100

    fish: change functionality of p function

--- .config/fish/functions/p.fish
+++ .config/fish/functions/p.fish
@@ -2,7 +2,8 @@ for project in (path basename ~/programmeren/*)
     complete -f -c p -a $project
 end
 
-function p
+function p --description "Open a programming project" -a project
     set -l P_FOLDER /Users/jonahmeijers/programmeren
-    cd "$P_FOLDER/$argv[1]"
+    test -n "$project"; or set -l project (path basename -Z $P_FOLDER/* | fzf --read0 --print0)
+    cd "$P_FOLDER/$project"
 end
