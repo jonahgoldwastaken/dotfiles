6076b0decb408073e6e917937eef67baff6b4efe
Jonah Meijers <jonahmeijers97@gmail.com>
2024-01-09 18:26:49 +0100

    fish: git aliases

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,11 +1,20 @@
+# Git
+alias gst "git status"
+alias gmd "git merge develop --no-edit"
+alias gbd "git branch -D"
+
+# Vim
 alias vim nvim
 alias vi nvim
 alias v nvim
 
+# Shell commands
 alias find fd
 alias cat bat
 
+# Killport
 alias kp killport
 
+# Clear
 alias cls clear
 alias cl clear
