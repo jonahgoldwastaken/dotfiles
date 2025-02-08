907a5756b1f1b58aa3a880bcfdeef3a531ff2095
Jonah Meijers <jonahmeijers97@gmail.com>
2025-02-08 23:58:29 +0100

    nvim: add keymaps for gitsigns

--- .config/nvim/lua/jonahgold/plugins/ui.lua
+++ .config/nvim/lua/jonahgold/plugins/ui.lua
@@ -114,6 +114,76 @@ return {
 		opts = {
 			current_line_blame = true,
 			signcolumn = true,
+			on_attach = function(bufnr)
+				local gitsigns = require "gitsigns"
+
+				local function map(mode, keymap, action, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, keymap, action, opts)
+				end
+
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal { "]h", bang = true }
+					else
+						gitsigns.nav_hunk "next"
+					end
+				end)
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal { "[h", bang = true }
+					else
+						gitsigns.nav_hunk "prev"
+					end
+				end)
+
+				-- Actions
+				map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage Hunk" })
+				map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset Hunk" })
+
+				map(
+					"v",
+					"<leader>hs",
+					function() gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" } end,
+					{ desc = "Stage Hunk" }
+				)
+
+				map(
+					"v",
+					"<leader>hr",
+					function() gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" } end,
+					{ desc = "Stage Hunk" }
+				)
+
+				map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Stage Buffer" })
+				map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Reset Buffer" })
+				map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview Hunk" })
+				map("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "Preview Hunk (inline)" })
+
+				map(
+					"n",
+					"<leader>hb",
+					function() gitsigns.blame_line { full = true } end,
+					{ desc = "Blame Line" }
+				)
+
+				map("n", "<leader>hd", gitsigns.diffthis, { desc = "Open Diff" })
+
+				map("n", "<leader>hD", function() gitsigns.diffthis "~" end, { desc = "Open Diff" })
+
+				map(
+					"n",
+					"<leader>hQ",
+					function() gitsigns.setqflist "all" end,
+					{ desc = "Set All Hunks In QFList" }
+				)
+				map("n", "<leader>hq", gitsigns.setqflist, { desc = "Set Current Hunk in QFList" })
+
+				-- Text object
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+			end,
 		},
 	},
 }
